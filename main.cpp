d6eaf58242ed27930e23f8f44f0b801de4e144a4
Johan Klokkhammer Helsing <johan.helsing@qt.io>
2018-04-16 13:03:42 +0200

    Read from stdin or file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QtCore/QTextStream>
+#include <QtCore/QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -8,7 +11,23 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    QString waylandLog;
+    if (app.arguments().size() < 2) {
+        qDebug() << "Reading log from stdin";
+        waylandLog = QTextStream(stdin).readAll();
+    } else if (app.arguments().size() == 2) {
+        QFile logFile(app.arguments().at(1));
+        if (!logFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            qCritical("Failed to open log file");
+            return EXIT_FAILURE;
+        }
+        waylandLog = QTextStream(&logFile).readAll();
+    }
+
     QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextProperty("waylandLog", waylandLog);
+
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
